b68e815e4ae8ef14f737cba0bc3201b9e2979ae5
aksharamin <akshar.amin@gs1.org>
2019-08-26 10:44:44 -0400

    Added Image to Landing and Changed ACCOUNT to INFO

--- screens/Info.js
+++ screens/Info.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 export default class Info extends React.Component{
   static navigationOptions = {
     headerStyle:{
-      backgroundColor: 'orange'
+      // backgroundColor: 'orange'
     }
   }
   render(){
@@ -12,7 +12,7 @@ export default class Info extends React.Component{
       <View style={styles.container}>
         <Text>About this app:</Text>
         <Text></Text>
-        <Text>This Application is a prototype  Barcode scanner created by GS1 global and powered by Verified by GS1</Text>
+        <Text>This Application is a prototype  Barcode scanner created by GS1 Global and powered by Verified by GS1</Text>
         <Text></Text>
         <Text>Application was built using React Native. Ran and tested through Expo. Barcode scanner is built by Expo's own Barcode scanner api</Text>
       </View>
